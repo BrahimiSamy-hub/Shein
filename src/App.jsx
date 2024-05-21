25007148c8e1709255c473af679365638d68bbb0
Samy <BrahimiSamy123@gmail.com>
2024-05-21 18:01:03 +0100

    added category page

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,41 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../src/pages/Home";
-import Arrivals from "../src/pages/Arrivals";
-import { CategoryProvider } from "./context/categoryContext";
-import { ArrivalProvider } from "./context/arrivalContext";
-import { ProductProvider } from "./context/productContext";
-import { CartProvider } from "./context/cartContext";
-import { UserProvider } from "./context/userContext";
-import { OrderProvider } from "./context/orderContext";
-import Login from "./pages/Login";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from '../src/pages/Home'
+import Arrivals from '../src/pages/Arrivals'
+import Login from './pages/Login'
+import Categories from './pages/Categories'
+import { CategoryProvider } from './context/categoryContext'
+import { ArrivalProvider } from './context/arrivalContext'
+import { ProductProvider } from './context/productContext'
+import { CartProvider } from './context/cartContext'
+import { UserProvider } from './context/userContext'
+import { OrderProvider } from './context/orderContext'
+import { ToastProvider } from './context/toastContext'
 
 const App = () => {
   return (
     <Router>
-      <UserProvider>
-        <CartProvider>
-          <OrderProvider>
-            <ArrivalProvider>
-              <CategoryProvider>
-                <ProductProvider>
-                  <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/" element={<Home />} />
-                    <Route path="/arrivals" element={<Arrivals />} />
-                  </Routes>
-                </ProductProvider>
-              </CategoryProvider>
-            </ArrivalProvider>
-          </OrderProvider>
-        </CartProvider>
-      </UserProvider>
+      <ToastProvider>
+        {/* <UserProvider>
+          <CartProvider>
+            <OrderProvider>
+              <ArrivalProvider> */}
+        <CategoryProvider>
+          {/* <ProductProvider> */}
+          <Routes>
+            {/* <Route path='/login' element={<Login />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/arrivals' element={<Arrivals />} /> */}
+            <Route path='/categories' element={<Categories />} />
+          </Routes>
+          {/* </ProductProvider> */}
+        </CategoryProvider>
+        {/* </ArrivalProvider>
+            </OrderProvider>
+          </CartProvider>
+        </UserProvider> */}
+      </ToastProvider>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
